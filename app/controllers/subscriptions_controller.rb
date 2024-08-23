61af65b5c63f9474a091667a0eb8f6b767314dfc
Julia Krebs <152524036+jcrabs@users.noreply.github.com>
2024-08-23 14:58:49 +0200

    Subscription requests are now displayed in appropriate categories.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,8 +1,10 @@
 class SubscriptionsController < ApplicationController
   def list
-    @active_subscriptions = Booking.joins(:offer).where(user_id: current_user.id, offers: { status: true })
-    @past_subscriptions = Booking.joins(:offer).where(user_id: current_user.id, offers: { status: false })
+    @active_subscriptions = Booking.joins(:offer).where(user_id: current_user.id, offers: { status: false })
+    @past_subscriptions = Booking.joins(:offer).where(user_id: current_user.id, status: "accepted", offers: { status: true })
 
     @pending_subscriptions = Booking.where(user_id: current_user.id, status: "pending")
+    @accepted_subscriptions = Booking.where(user_id: current_user.id, status: "accepted")
+    @rejected_subscriptions = Booking.where(user_id: current_user.id, status: "rejected")
   end
 end
